27154e249130e8e329ca3a0fcef7663e13b80370
ふぁ <yuki@yuki0311.com>
2022-11-10 17:27:12 +0900

    update riverpod
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sns_speed_test/widget/speed_test.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       title: 'Speed Test',
       home: SpeedTestWidget(),
